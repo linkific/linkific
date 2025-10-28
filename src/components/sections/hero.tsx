080812a1e01d32da8dd34510dc2bcab98b23763d
Harshal Bondre <harshalbondre101@gmail.com>
2025-10-28 18:03:40 +0000

    cant see doodle

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
 
 export default function HeroSection() {
     const [scrollY, setScrollY] = useState(0);
