81b9716247b571634e638b58a4514ed6d8975894
Harshal Bondre <harshalbondre101@gmail.com>
2025-10-29 05:50:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8367233579-8eb47",
+  "appId": "1:517258943779:web:9e9bb49b48e792e62f4096",
+  "apiKey": "AIzaSyCGtGkX-SKei2uREhlM5PaDf9gP-OjgEUY",
+  "authDomain": "studio-8367233579-8eb47.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "517258943779"
+};
