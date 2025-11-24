e6855bd733afa73c0f8cd4d20bce9c113f3fe166
Harshal Bondre <harshalbondre101@gmail.com>
2025-11-24 17:30:54 +0000

    keep white but it shouldnt hurt eyes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const config = {
         'sky-blue': '#B3CFE5',
         'steel-blue': '#4A7FA7',
         'deep-blue': '#1A3D63',
-        'off-white': '#dcf3ff',
+        'off-white': '#F0F2F5',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
